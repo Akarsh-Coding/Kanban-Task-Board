519822fcd76c2b7e200f7b0165a6791d8f4ab2a8
Akarsh-Coding <akarsh4.pvt@gmail.com>
2026-07-21 09:36:29 +0530

    Initialize React project with Vite

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <dir>
+        <h1>KANBAN TASK BOARD</h1>
+      </dir>
+    </>
+  )
+}
+
+export default App
